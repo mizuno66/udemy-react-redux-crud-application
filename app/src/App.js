2ecd6a48a554a753e173f2f519191db37595eee9
Mizuno66 <kouta126@hotmail.co.jp>
2020-01-27 21:41:13 +0900

    lean JSX

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello,World!</h1>
-      </div>
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Hello,World!
+//       </div>
+//     );
+//   }
+// }
+
+// jsxを使わない場合の書き方
+// jsxを利用する場合は、トランスパイラーにて上記から下記に変換される
+class App extends Component{
+  render(){
+    return React.createElement(
+      "h1",
+      null,
+      "Hello, world!!"
     );
   }
 }
